c6a982bca626031fc21efd1a1bc54fa6af2bb0e3
itayush <87350108+Yati2@users.noreply.github.com>
2024-11-09 01:19:16 +0800

    Update Pet.vue

--- frontend/src/components/Pet.vue
+++ frontend/src/components/Pet.vue
@@ -4,7 +4,7 @@
       <img src="/assets/loadingcat.gif" alt="Loading" class="loading-gif" />
       <h3 class="loading-text">Loading...</h3>
     </div>
-    <div v-else class="row">
+    <div class="row">
       <div id="game-container" ref="gameContainer" class="col-lg-10 col-12 position-relative">
         <img id="game-bg" class="position-absolute" />
       </div>
